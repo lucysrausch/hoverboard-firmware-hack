bd4d590303150e56a04ebda1ea24a93761c38409
tomv <hover@vocke.tv>
2018-04-25 17:02:02 -0700

    modbus now using function pointers to allow easy connection to other implementations

--- Inc/cfgbus.h
+++ Inc/cfgbus.h
@@ -1,9 +1,35 @@
-#pragma once
-
 #ifndef CFGBUS_H
 #define CFGBUS_H
 
 #include <stdint.h>
+
+//address at which the cfgbus list can be requested
+#define CFG_LIST_REG_INDEX  (5000)   //excl 40001 range offset
+#define CFG_DEVICE_NAME     "Hover"  //10 chars max
+
+//I am not a big fan of complicated macro structures, but this was the only way I could think
+//of that allows an easy definition of entries, that can be easily accessed (cfg.vars._varname),
+//and that leaves no room for error by multiple definitions of the same thing
+
+//assign initial values in CfgInit()
+
+//---------------------------------------------------------------------------------------
+//FORMAT      | var name       | type      | cfg_type | writeable | name string         |
+//---------------------------------------------------------------------------------------
+#define CFG_ENTRIES(_ENTRY) \
+        _ENTRY( magic          , uint32_t  , t_u32    , 0         , "Magic Value"       ) \
+        _ENTRY( dev_name[12]   , char      , t_str    , 0         , "Device Name"       ) \
+        _ENTRY( err_code       , uint16_t  , t_u16    , 1         , "Error Code"        ) \
+        _ENTRY( err_cnt        , uint16_t  , t_u16    , 1         , "Error Count"       ) \
+        _ENTRY( list_size      , uint16_t  , t_u16    , 0         , "List Size"         ) \
+        _ENTRY( list_addr      , uint16_t  , t_u16    , 0         , "List Address"      ) \
+        _ENTRY( pwm_l          , uint32_t  , t_u32    , 1         , "Left-Mtr PWM"      ) \
+        _ENTRY( pwm_r          , uint32_t  , t_u32    , 1         , "Right-Mtr PWM"     ) \
+        _ENTRY( vbat           , float     , t_flt    , 0         , "Battery Voltage"   )
+
+//---------------------------------------------------------------------------------------
+// DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING
+//---------------------------------------------------------------------------------------
 
 uint32_t CfgAvailable();
 int CfgRead(uint8_t * data, uint32_t len);
@@ -11,6 +37,10 @@ int CfgWrite(uint8_t * data, uint32_t len);
 void CfgFlushRx();
 void CfgFlushTx();
 uint32_t CfgTick();
+void CfgSetError(int error);
+void CfgRegRead(uint16_t start, uint16_t nr_regs, uint8_t* data);
+void CfgRegWrite(uint16_t start, uint16_t nr_regs, uint8_t* data);
+int CfgValidRange(uint16_t first, uint16_t cnt);
 
 typedef enum
 {
@@ -30,61 +60,51 @@ typedef enum
 
 typedef enum
 {
-  t_u8    = 0,
-  t_u16   = 1,
-  t_u32   = 2,
-  t_u64   = 3,
-  t_i8    = 4,
-  t_i16   = 5,
-  t_i32   = 6,
-  t_i64   = 7,
-  t_flt   = 8,
-  t_dbl   = 9,
-  t_bool  = 10,
-  t_str   = 11,
-  t_err   = 12,
-  t_unknown = 13//should be last
+  t_u16   = 0,
+  t_u32   = 1,
+  t_u64   = 2,
+  t_i16   = 3,
+  t_i32   = 4,
+  t_i64   = 5,
+  t_flt   = 6,
+  t_dbl   = 7,
+  t_bool  = 8,
+  t_str   = 9,
+  t_err   = 10,
+  t_unknown = 11//should be last
 } cfg_type_t;
 
-//should match the amound of entries in the cfg_t struct.
-#define CFG_NUM_ENTRIES (6)
+//DO NOT TOUCH, used to generate cfg struct, entry list, and count number of entries
+#define _STRUCT(_var,_type,_1,_2,_3) _type _var;
+#define _LIST(_var,_1,_type,_rw,_name) {((uint16_t*)&cfg.vars._var)-&cfg.regs[0], sizeof(cfg.vars._var), _type, _rw, _name},
+#define _COUNT(_1,_2,_3,_5,_6) +1
 
-typedef enum
-{
-  false = 0,
-  true  = !false
-} bool_t;
+#define CFG_NR_ENTRIES (0+CFG_ENTRIES(_COUNT))
 
 typedef struct
 {
-  volatile void *addr;
+  uint8_t index;
   uint8_t size;
   uint8_t type; //types declared in "type_t" enum
   uint8_t writeable; //0=no, !0= yes
-  const char* name;
+  const char name[16];
 } cfg_entry_t;
 
 typedef struct
 {
-  //cfgbus default entries, do not remove
-  uint32_t magic;      //verify CFGBus compatibility
-  char dev_name[12];
-  uint8_t err_code;
-  uint16_t err_cnt;
-  uint16_t list_size;
-  uint16_t list_addr;
-
-  //user entries
-
-  uint32_t pwm_l;
-  uint32_t pwm_r;
-  float vbat;
+  CFG_ENTRIES(_STRUCT)
 } cfg_t;
 
-extern volatile cfg_t cfg;
-extern const cfg_entry_t cfg_entries[CFG_NUM_ENTRIES];
+//defined as union so that struct members will be placed in same memory
+//as configbus registers, only multiple of 2-byte types allowed to ensure
+//allignment with registers
+typedef union{
+  cfg_t    vars;
+  uint16_t regs[(sizeof(cfg_t)+1)/2];
+} cfg_mem_t;
 
-
+extern volatile cfg_mem_t cfg;
+extern const  cfg_entry_t cfg_entries[CFG_NR_ENTRIES];
 
 #endif //CFGBUS_H
 
--- Inc/modbus.h
+++ Inc/modbus.h
@@ -10,19 +10,21 @@
 
 #pragma once
 
-
-
 #include <stdint.h>
 
-#define MODBUS_BROADCAST_ADDRESS 0
+#define MB_CHAR_TIMEOUT   (1)  //max ms between chars
+#define MB_PACKET_TIMEOUT (3)  //max ms between chars
+#define MB_BROADCAST_ADDR (0)
+#define MB_RX_BUFFER_SIZE (128)
+#define MB_SLAVE_ID       (16)
 
 /* Protocol exceptions */
 typedef enum
 {
   mb_illegal_func    = 1,
   mb_illegal_address = 2,
-  mb_illega_value    = 3
-
+  mb_illegal_value    = 3,
+  mb_timeout         = 4
 } mb_exception_t;
 
 void modbusUpdate();
