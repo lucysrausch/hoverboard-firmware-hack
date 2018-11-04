398f5da7b93e184992a8faf988d08fa403a52995
jankosso <jan_kossowski@web.de>
2018-11-04 17:33:44 +0100

    Modified to compile under SW4STM32: -Added comms.h with declarations -included stdlib.h in main() für abs()
    Updated pinout.png to reflect UART number for RX and TX

---
+++ Inc/comms.h
@@ -0,0 +1,15 @@
+/*
+ * comms.h
+ *
+ *  Created on: 20.04.2018
+ *      Author: Jan
+ */
+
+#ifndef INC_COMMS_H_
+#define INC_COMMS_H_
+
+void setScopeChannel(uint8_t ch, int16_t val);
+void consoleScope();
+void consoleLog(char *message);
+
+#endif /* INC_COMMS_H_ */
