9cf4988cdec736ca46aaa1b1232b338ff9dd7fe9
EmanuelFeru <aerdronix@gmail.com>
2019-06-26 19:28:35 +0200

    cleanup

--- Inc/rtwtypes.h
+++ Inc/rtwtypes.h
@@ -1,7 +1,4 @@
 /*
- * Academic License - for use in teaching, academic research, and meeting
- * course requirements at degree granting institutions only.  Not for
- * government, commercial, or other organizational use.
  *
  * File: rtwtypes.h
  *
